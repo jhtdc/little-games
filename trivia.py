a4599cf4c1a6519b87fac54e63fe3c3ad803d1eb
Jason Towns <jasonhtowns@gmail.com>
2014-04-30 10:44:11 -0400

    Added first game

---
+++ trivia.py
@@ -0,0 +1,47 @@
+# trivia.py
+# This is a simple trivia game based on facts about black inventors.
+
+
+# Welcome Screen
+print "======== Welcome To 'Did They Do That?' ========"
+
+
+#Section 1. The first question
+print "\nDid a black inventor create the original Super Soaker water gun?"
+print  "\n(a) no way \t(b) yes \t(c) I have absolutely no idea"
+
+answer = raw_input("Your answer:")
+
+if answer =="b":
+	print "Trueee! Lonnie George Johnson, a black inventor and NASA engineer, created the Super Soaker. It was the top selling toy in the US in 1991 and 1992."
+else:
+	print "Nope! Sorry, but you were wrong here. Lonnie George Johnson, a black inventor, created the Super Soaker. It was the top selling toy in the US in 1991 and 1992.\n"
+	
+
+#Section 2. The second question
+print "\nThis person invented an early version of the spark plug which helped drive innovation in the auto industry"
+print  "\n(a) Henry Blair \t(b) Tony Blair \t(c) Blair Witch"
+
+answer = raw_input("Your answer:")
+
+if answer =="a":
+	print "You nailed it! Henry Blair was the black inventor who created an early version of the spark plug. He was also the second black inventor to issue a patent. This dude was a true innovator!"
+else:
+	print "Nope, nice try though! Henry Blair, a black inventor, created an early version of the spark plug. He was also the second black inventor to issue a patent. This dude was a true innovator!\n"
+	
+#Section 3. The third question
+print "\nThis person invented an early version of the spark plug which helped drive innovation in the auto industry"
+print  "\n(a) Henry Blair \t(b) Tony Blair \t(c) Blair Witch"
+
+answer = raw_input("Your answer:")
+
+if answer =="a":
+	print "You nailed it! Henry Blair was the black inventor who created an early version of the spark plug. He was also the second black inventor to issue a patent. This dude was a true innovator!"
+else:
+	print "Nope, nice try though! Henry Blair, a black inventor, created an early version of the spark plug. He was also the second black inventor to issue a patent. This dude was a true innovator!\n"
+	
+
+
+
+
+
