e96ac491842a41b40a3ce3d801ca13d9f9d74eaa
Jason Towns <jasonhtowns@gmail.com>
2014-06-07 14:53:51 -0400

    updated __init.py__, debugging.

--- progress_player/app/__init__.py
+++ progress_player/app/__init__.py
@@ -8,12 +8,28 @@ it and if you're using a single module (as in this example), use __name__ with t
 
 Next there's the UPLOAD_FOLDER where you will store the uploaded files 
 and then in the final line we're configuring the app variable with the UPLOAD_FOLDER.
+
+Still having issues getting the uploaded file to load correctly and I'm suspecting it's something
+related to the syntax surrounding the UPLOAD_FOLDER variable. 
+The commented out code is what I started with. Got ioerror when loading mp3. 
+The current code gets me one step further and loads the player but still, no playback.
+This is probably the worlds longest comment in a basic ass __init__.py file.
 '''
 
-from flask import Flask
+'''from flask import Flask
 app = Flask (__name__)
 UPLOAD_FOLDER = r'./uploads'
 app.config ['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-from app import views
+from app import views'''
 
+import os
+from flask import Flask
+ 
+app = Flask(__name__)
+ 
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = os.path.join(APP_ROOT, 'static/uploads')
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+from app import views
