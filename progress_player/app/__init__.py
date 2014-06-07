2835a5a68cacec0b5fcfa0ce4735710ecbab7a1f
Jason Towns <jasonhtowns@gmail.com>
2014-06-07 12:18:44 -0400

    added bootstrap css files and updated app skeleton

--- progress_player/app/__init__.py
+++ progress_player/app/__init__.py
@@ -1,14 +1,19 @@
 '''
-I imported the Flask class and then create an 
-instance of Flask class and created an object of 
-it and if I'm using a single module (as in this example), 
-I should use __name__ with the class.
+This flask skeleton file stays empty alot of the time.
+However, it can be used to import packages or define classes/functions.
 
-Next there's the UPLOAD_FOLDER where I will store the uploaded files 
-and then in the final line I'm configuring the app variable with the UPLOAD_FOLDER.
+So here we import the Flask class and then create an 
+instance of Flask class and created an object of 
+it and if you're using a single module (as in this example), use __name__ with the class.
+
+Next there's the UPLOAD_FOLDER where you will store the uploaded files 
+and then in the final line we're configuring the app variable with the UPLOAD_FOLDER.
 '''
 
-from flask import flask
+from flask import Flask
 app = Flask (__name__)
 UPLOAD_FOLDER = r'./uploads'
 app.config ['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+from app import views
+
---
+++ progress_player/run.py
@@ -0,0 +1,7 @@
+# comments
+
+
+from app import app
+
+app.run(debug = True)
+
