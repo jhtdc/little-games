3027433a4120c9d36abfb0b5eb48e1102a417e88
Jason Towns <jasonhtowns@gmail.com>
2014-07-16 07:05:49 -0400

    debugging/ works now

---
+++ progress_player/app/settings.py
@@ -0,0 +1,4 @@
+import os
+
+APP_PATH = os.path.dirname(os.path.abspath(__file__))
+STATIC_PATH = os.path.join(APP_PATH, 'static')
--- progress_player/run.py
+++ progress_player/run.py
@@ -1,7 +1,7 @@
-# comments
+#  I enabled debug support by passing it as a parameter so the server will reload itself on code changes, and provide me with a helpful debugger if things go wrong.
 
 
 from app import app
 
-app.run(debug = True)
+app.run(debug=True, port=8000)
 
